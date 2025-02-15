abd282e7b4be6651b45165608b57ab40add653ee
AndrewKaranu <andrewkaranu03@gmail.com>
2025-02-15 18:39:46 +0000

    Prepared the main for merging

--- aoe_tower.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "tower.h"
-
-class AoETower : public Tower {
-public:
-    AoETower();
-    void attack(std::vector<Critter*>& targets) override;
-};
--- base_tower.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "tower.h"
-
-class BasicTower : public Tower {
-public:
-    BasicTower();
-    void attack(std::vector<Critter*>& targets) override;
-};
